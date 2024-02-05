2168790d99c0c2bbe451e7b81e1013e753972fdf
Alexander <irdaqq@gmail.com>
2024-02-05 11:36:22 +0400

    1.0.199

--- bridge-api-definition.js
+++ bridge-api-definition.js
@@ -10,8 +10,8 @@ tuncheeky.DC_API_UNSUPPORTED = "UNSUPPORTED";
 tuncheeky.DC_API_READY = "READY";
 tuncheeky.DC_API_FAILED = "FAILED";
 
-tuncheeky.DT_DESKTOP = "desktop";
-tuncheeky.DT_MOBILE = "mobile";
+tuncheeky.DC_DESKTOP = "DESKTOP";
+tuncheeky.DC_MOBILE = "MOBILE";
 
 //
 // Config
@@ -28,85 +28,103 @@ tuncheeky.config = {
 };
 
 //
-// Common API
-//
-
-tuncheeky.api.textInputCallback = null;
-tuncheeky.api.textInputUpdated = false;
-
-tuncheeky.api.callTextInputCallback = function(action, data) {
-    const overlay = document.getElementById("text-input-window-overlay");
-    overlay.style.display = "none";
-
-    tuncheeky.api.textInputUpdated = false;
-
-    const callback = tuncheeky.api.textInputCallback;
-    tuncheeky.api.textInputCallback = null;
-    if (callback != null) {
-        callback(action, data);
-    }
-};
-
-tuncheeky.api.showTextInputWindow = function(initialValue, callback) {
-    const overlay = document.getElementById("text-input-window-overlay");
-    const textField = document.getElementById("text-input-window-text-field");
-
-    overlay.style.display = "block";
-    textField.value = initialValue;
-    textField.focus();
-
-    tuncheeky.api.textInputUpdated = false;
-    tuncheeky.api.textInputCallback = callback;
-};
-
-//
 // Bridge API
 //
 
-// Actual API
+tuncheeky.api = {
+    textInputCallback: null,
+    textInputUpdated: false,
 
-tuncheeky.api.initialize = function(callback) {
-    callback(tuncheeky.DC_API_UNSUPPORTED);
-};
+    // Common API
 
-// Distribution Channel API
+    initializeCommonApi() {
+        const tapi = this;
 
-tuncheeky.api.getDeviceCategory = function() {
-    throw new Error("Unsupported operation: getDeviceCategory()");
-};
+        document.addEventListener("DOMContentLoaded", () => {
+            document.getElementById("text-input-window-overlay").addEventListener("click", (event) => {
+                if (event.target.id == "text-input-window-overlay") {
+                    if (!tapi.textInputUpdated) {
+                        tapi.callTextInputCallback("cancel", null);
+                    }
+                }
+            });
+            document.getElementById("text-input-window-text-field").addEventListener("input", (event) => {
+                tapi.textInputUpdated = true;
+            });
+            document.getElementById("text-input-window-button-enter").addEventListener("click", (event) => {
+                const textField = document.getElementById("text-input-window-text-field");
+                const value = textField.value;
+                tapi.callTextInputCallback("enter", value);
+            });
+            document.getElementById("text-input-window-button-cancel").addEventListener("click", (event) => {
+                tapi.callTextInputCallback("cancel", null);
+            });
+            // tapi.showTextInputWindow("Initial text", (a, v) => alert(`${a}, ${v}`));
+        });
+    },
 
-tuncheeky.api.isUseNativeTextInputMethod = function() {
-    throw new Error("Unsupported operation: isUseNativeTextInputMethod()");
-};
+    callTextInputCallback(action, data) {
+        const overlay = document.getElementById("text-input-window-overlay");
+        overlay.style.display = "none";
 
-tuncheeky.api.showFullScreenAd = function() {
-    throw new Error("Unsupported operation: showFullScreenAd()");
-};
+        this.textInputUpdated = false;
+
+        const callback = this.textInputCallback;
+        this.textInputCallback = null;
+        if (callback != null) {
+            callback(action, data);
+        }
+    },
+
+    showTextInputWindow(initialValue, callback) {
+        const overlay = document.getElementById("text-input-window-overlay");
+        const tiWindow = document.getElementById("text-input-window");
+        const textField = document.getElementById("text-input-window-text-field");
+
+        overlay.style.display = "block";
+        overlay.classList.remove("show-text-input-window-overlay-animation");
+        overlay.classList.add("show-text-input-window-overlay-animation");
+
+        textField.value = initialValue;
+        textField.focus();
+
+        tiWindow.classList.remove("show-text-input-window-animation");
+        tiWindow.classList.add("show-text-input-window-animation");
+
+        this.textInputUpdated = false;
+        this.textInputCallback = callback;
+    },
+
+    // Actual API
+
+    initialize(callback) {
+        callback(tuncheeky.DC_API_UNSUPPORTED);
+    },
+
+    getDeviceCategory() {
+        return throwUnsupportedOperation("getDeviceCategory");
+    },
+
+    isUseNativeTextInputMethod() {
+        return throwUnsupportedOperation("isUseNativeTextInputMethod");
+    },
+
+    notifyGameLoaded() {
+        throwUnsupportedOperation("notifyGameLoaded");
+    },
+
+    showFullScreenAd() {
+        throwUnsupportedOperation("showFullScreenAd");
+    },
+
+    throwUnsupportedOperation(operation) {
+        throw new Error(`Unsupported operation: ${operation}`);
+    }
+
+}
 
 //
 // Bridge API Initializer
 //
 
-(function() {
-    document.addEventListener("DOMContentLoaded", () => {
-        document.getElementById("text-input-window-overlay").addEventListener("click", (event) => {
-            if (event.target.id == "text-input-window-overlay") {
-                if (!tuncheeky.api.textInputUpdated) {
-                    tuncheeky.api.callTextInputCallback("cancel", null);
-                }
-            }
-        });
-        document.getElementById("text-input-window-text-field").addEventListener("input", (event) => {
-            tuncheeky.api.textInputUpdated = true;
-        });
-        document.getElementById("text-input-window-button-enter").addEventListener("click", (event) => {
-            const textField = document.getElementById("text-input-window-text-field");
-            const value = textField.value;
-            tuncheeky.api.callTextInputCallback("enter", value);
-        });
-        document.getElementById("text-input-window-button-cancel").addEventListener("click", (event) => {
-            tuncheeky.api.callTextInputCallback("cancel", null);
-        });
-    });
-})();
-
+tuncheeky.api.initializeCommonApi();
